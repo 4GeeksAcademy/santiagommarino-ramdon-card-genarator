3a70aa605a63100ca0e3f0b13076656062775871
santiagommarino <153780787+santiagommarino@users.noreply.github.com>
2024-04-04 14:41:28 +0000

    cards done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,45 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pickSuit = () => {
+  let suits = ["♥", "♦", "♠", "♣"];
+  let randomIndex = Math.floor(Math.random() * suits.length);
+  return suits[randomIndex];
+};
+
+let pickCardNumber = () => {
+  let cardNumbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let randomIndex = Math.floor(Math.random() * cardNumbers.length);
+  return cardNumbers[randomIndex];
+};
+let generateDisplayCard = () => {
+  let suit = pickSuit();
+  let cardNumber = pickCardNumber();
+  let topIcon = document.querySelector(".top-icon");
+  let bottomIcon = document.querySelector(".bottom-icon");
+  let cardContent = document.querySelector(".card-content p");
+
+  topIcon.innerHTML = suit;
+  bottomIcon.innerHTML = suit;
+  cardContent.innerHTML = `<strong>${cardNumber}</strong>`;
+};
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateDisplayCard();
+  document
+    .getElementById("generateCard")
+    .addEventListener("click", generateDisplayCard);
 };
